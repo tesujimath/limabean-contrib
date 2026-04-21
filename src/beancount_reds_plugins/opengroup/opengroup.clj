1368972efcf8eca124c5763d013059d34d049669
Simon Guest <simon.guest@tesujimath.org>
2026-04-21 17:09:26 +1200

    Got plugin working

--- src/beancount_reds_plugins/opengroup/opengroup.clj
+++ src/beancount_reds_plugins/opengroup/opengroup.clj
@@ -1,8 +1,9 @@
 (ns beancount-reds-plugins.opengroup.opengroup
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [limabean.plugin :as plugin]
+            [selmer.parser :as selmer]))
 
-;; TODO ^{:private true}
-(def DEFAULT-RULES
+(def ^{:private true} DEFAULT-RULES
   {"cash_and_fees"
      [; Open cash and fees accounts
       #"(?<root>[^:]*):(?<subroot>[^:]*):(?<taxability>[^:]*):(?<accountname>.*)"
@@ -35,8 +36,8 @@
        ["Income:{{subroot}}:{{taxability}}:Capital-Gains-Distributions:Short:{{accountname}}:{{f_ticker}}"
         "{{f_opcurr}}"]]]})
 
-(defn opengroup_meta_rule_leaves
-  "Return [rule, [leaves]] for any metadata strings whose name starts opengroup_"
+(defn- get-meta-rule-leaves
+  "Return [rulename [leaves]] for any metadata strings whose name starts opengroup_"
   [dct]
   (keep (fn [[k v]]
           (let [k-name (name k)
@@ -46,18 +47,37 @@
                (str/split v-string #",")])))
         (:metadata dct)))
 
-(defn warn [& args] (binding [*out* *err*] (apply println args)))
-
-(defn re-matched-groups
+(defn- re-matched-groups
   [m]
   (and m (into {} (map (fn [[name i]] [name (.group m i)]) (.namedGroups m)))))
 
-(defn run-rules
-  [rules rulename f_acct f_ticker f_opcurr entry]
-  (let [[rule inserts] (get rules rulename)]
+(defn- run-rule
+  [rules source rulename dct ticker op-cur]
+  (let [acc (:acc dct)
+        [rule inserts] (get rules rulename)]
     (if rule
-      (let [m (re-matcher rule f_acct)] (and (re-find m) m))
-      (warn "rule" rulename "not found"))))
+      (let [m (re-matcher rule acc)]
+        (and (re-find m)
+             (let [g (assoc (update-keys (re-matched-groups m) keyword)
+                       :f_acct acc
+                       :f_ticker ticker
+                       :f_opcurr op-cur)]
+               (map (fn [insert]
+                      (let [[acc cur] (map #(selmer/render % g) insert)]
+                        (assoc (select-keys dct [:dct :date])
+                          :acc acc
+                          :cur cur
+                          :metadata {:auto nil})))
+                 inserts))))
+      (plugin/error! dct (str "rule " rulename " not found in " source)))))
+
+(defn- run-rule-for-leaves
+  [rules source [rulename leaves] dct op-cur]
+  (mapcat #(run-rule rules source rulename dct % op-cur) leaves))
+
+(defn- run-rules-for-group
+  [rules source group dct op-cur]
+  (mapcat #(run-rule-for-leaves rules source % dct op-cur) group))
 
 (defn raw-xf
   "Port from original plugin in Python with the same license, GPL 3.0.
@@ -71,7 +91,8 @@
   "
   [{:keys [config options]}]
   (let [rules (or config DEFAULT-RULES)
-        op-currency (or (first (:operating-currency options)) "USD")]
+        source (if config "plugin config" "default rules")
+        op-cur (or (first (:operating-currency options)) "USD")]
     (fn [rf]
       (fn
         ;; init
@@ -80,10 +101,13 @@
         ([result] (rf result))
         ;; step
         ([result dct]
-         (rf result
-             (let [result' (rf result dct)]
-               (if (= (:dct dct) :open)
-                 (reduce (fn [result'' [rule leaves]])
-                   result'
-                   (opengroup_meta_rule_leaves dct))
-                 result'))))))))
+         (let [result' (rf result dct)]
+           (if (= (:dct dct) :open)
+             (reduce rf
+               result'
+               (run-rules-for-group rules
+                                    source
+                                    (get-meta-rule-leaves dct)
+                                    dct
+                                    op-cur))
+             result')))))))
